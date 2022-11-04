e3bb7972ddc15cd926993c8010f77da234095bea
Adam Warren <113055287+1031warren@users.noreply.github.com>
2022-11-04 17:08:15 -0600

    Add files via upload

---
+++ phun.js
@@ -0,0 +1,21 @@
+function arryTest1(){
+    var myClasses = ["CSC102", "CIS100", "ENG101"];
+    console.log(myClasses[1]);
+    myClass[1] = "CSC230";
+    console.log(myClasses.length);
+    for(var i= 0; i < myClasses.length; i++){
+        console.log(myClasses[i]);
+    }
+}
+
+function stackPhun(){
+    let myStack = [];
+    myStack.push("first items");
+    myStack.push("2nd item");
+    myStack.push("3rd item");
+    console.log(myStack);
+    for(var i = 0; i < myStack.length; i++){
+    }
+    myStack.pop();
+    console.log(myStack);
+}
